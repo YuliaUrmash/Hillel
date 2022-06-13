dc1590da0a263d0a51ec63da51835177bd9cd720
Yulia Urmash <yuliaurmash@gmail.com>
2022-06-13 12:59:40 +0300

    create hw8

---
+++ hw8_SingleResponsibility.php
@@ -0,0 +1,45 @@
+<?php
+
+class Product
+{
+    public function get($name)
+    {
+        //
+    }
+
+    public function set($name, $value)
+    {
+        //
+    }
+}
+
+class TreatmentProduct
+{
+    public function save(Product $product)
+    {
+        //
+    }
+
+    public function update(Product $product)
+    {
+        //
+    }
+
+    public function delete(Product $product)
+    {
+        //
+    }
+}
+
+class ViewProduct
+{
+    public function show(Product $product)
+    {
+        //
+    }
+
+    public function print(Product $product)
+    {
+        //
+    }
+}
